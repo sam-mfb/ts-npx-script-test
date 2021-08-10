b1a731a312bda0ead51f9beb0291ac66a91d33f7
sam-mfb <sam@mfbtech.com>
2021-08-10 16:14:46 -0400

    add comments

--- utils/foo/src/index.ts
+++ utils/foo/src/index.ts
@@ -1,4 +1,9 @@
-import "src/../node_modules/ts-jest"
+//import "../node_modules/ts-jest"
+
+// no auto-import suggestions for this, _unless_ you uncomment the 
+// import statement above (or any similar import statement for
+// a package, e.g., import "../node_modules/typescript" will work too)
+useEffect()
 
 export function myFunction() {
   const foo = "hello, world"
